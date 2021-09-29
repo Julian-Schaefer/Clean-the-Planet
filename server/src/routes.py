bb130f53ea6a6cd04fe59435fc57bb843cca9389
Julian Schäfer <julian.david.schaefer@gmail.com>
2021-09-29 20:22:09 +0200

    Added Server

---
+++ server/src/routes.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, request, jsonify
+import sqlalchemy
+from geoalchemy2 import functions
+
+from database import db
+from tour import Tour
+
+routes = Blueprint('Routes', __name__)
+
+
+@routes.route("/tour", methods=["POST"])
+def addTour():
+    #userId = request.user['user_id']
+    userId = "testi"
+
+    if request.is_json:
+        data = request.get_json()
+        tour = Tour(
+            userId=userId, polygon=data['polygon'], polyline=data['polygon'])
+        db.session.add(tour)
+        
+        try:
+            db.session.commit()
+        except sqlalchemy.exc.InternalError as err:
+            print(err)
+            return {"error": "Could not insert Tour into database."}, 400
+
+        return {"message": f"Tour {tour.id} for User {tour.userId} has been created successfully."}
+    else:
+        return {"error": "The request payload is not in JSON format"}, 400
+
+
+@routes.route("/tour", methods=["GET"])
+def getTours():
+    #userId = request.user['user_id']
+    userId = "testi"
+
+    tours = db.session.query(
+        Tour.id, functions.ST_AsText(Tour.polygon), functions.ST_AsText(Tour.polyline)).filter_by(userId=userId)
+
+    results = [
+        {
+            "id": id,
+            "polygon": polygon,
+            "polyline": polyline
+        } for (id, polygon, polyline) in tours]
+
+    return jsonify(results)
---
+++ server/src/tour.py
@@ -0,0 +1,21 @@
+from database import db
+from geoalchemy2 import Geometry
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+class Tour(db.Model):
+    __tablename__ = 'tour'
+
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    userId = db.Column(db.String())
+    polygon = db.Column(Geometry('POLYGON'))
+    polyline = db.Column(Geometry('POLYGON'))
+
+    def __init__(self, userId, polygon, polyline):
+        self.userId = userId
+        self.polygon = polygon
+        self.polyline = polyline
+
+    def __repr__(self):
+        return f"<Tour {self.id},  {self.userId}>"
