8bf7d7bf5a04da0f4cf9539378831fb6e4a834df
Julian Schäfer <julian.david.schaefer@gmail.com>
2021-10-07 17:28:12 +0200

    Added AWS_Region

--- server/src/utils.py
+++ server/src/utils.py
@@ -9,11 +9,13 @@ BUCKET = "clean-the-planet"
 ENDPOINT_URL = None
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', None)
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', None)
+AWS_REGION = os.environ.get('AWS_REGION', None)
 USE_SSL = True
-if not (AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY):
+if not (AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY and AWS_REGION):
     ENDPOINT_URL = "https://clean-the-planet-s3.loca.lt/"
     AWS_ACCESS_KEY_ID = '123'
     AWS_SECRET_ACCESS_KEY = 'abc'
+    AWS_REGION = None
     USE_SSL = False
 
 bucket = "clean-the-planet"
@@ -21,6 +23,7 @@ s3_resource = boto3.resource('s3',
                              endpoint_url=ENDPOINT_URL,
                              aws_access_key_id=AWS_ACCESS_KEY_ID,
                              aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                             region_name=AWS_REGION,
                              use_ssl=USE_SSL)
 
 signature_config = Config(signature_version='v4')
@@ -28,5 +31,6 @@ s3_client = boto3.client('s3',
                          endpoint_url=ENDPOINT_URL,
                          aws_access_key_id=AWS_ACCESS_KEY_ID,
                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                         region_name=AWS_REGION,
                          use_ssl=USE_SSL,
                          config=signature_config)
