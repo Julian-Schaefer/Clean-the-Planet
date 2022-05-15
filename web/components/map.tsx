06a8cf37a801dd8302c833e164e341cb75431cd5
Julian Schäfer <julian.david.schaefer@gmail.com>
2022-05-15 19:19:56 +0200

    Loading Statistics in Web

--- web/components/map.tsx
+++ web/components/map.tsx
@@ -1,31 +1,7 @@
-import { MapContainer, Marker, Popup, TileLayer, Circle } from 'react-leaflet'
-import L, { LatLngTuple } from 'leaflet'
+import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet'
+import L, { LatLng, LatLngBounds, LatLngTuple } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
-import { useState, useEffect } from 'react';
-import React from 'react';
-
-function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-        width,
-        height
-    };
-}
-
-export function useWindowDimensions() {
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-    useEffect(() => {
-        function handleResize() {
-            setWindowDimensions(getWindowDimensions());
-        }
-
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    return windowDimensions;
-}
+import React, { useEffect, useState } from 'react';
 
 interface CircleWithNumberProps {
     width: number;
@@ -46,48 +22,141 @@ const CircleWithNumber = (props: CircleWithNumberProps) => {
     })}></Marker>
 }
 
+function getLineString(bounds: LatLngBounds) {
+    let polylineString = "LINESTRING(";
+    for (var coord of [bounds.getNorthWest(), bounds.getSouthEast()]) {
+        polylineString +=
+            coord.lat.toString() + " " + coord.lng.toString() + ",";
+    }
+
+    polylineString = polylineString.substring(0, polylineString.length - 1);
+
+    return polylineString + ")";
+}
+
+function fromPointString(pointString: string): LatLng {
+    pointString = pointString.substring("POINT(".length);
+    pointString = pointString.substring(0, pointString.length - 1);
+
+    const coords = pointString.split(" ");
+    const latitude = Number(coords[0]);
+    const longitude = Number(coords[1]);
+    return new LatLng(latitude, longitude);
+}
+
+interface Statistic {
+    id: string;
+    address: string;
+    centerPoint: LatLng;
+    count: number;
+}
+
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+        width,
+        height
+    };
+}
+
+function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowDimensions;
+}
+
+function StatisticMarkers() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [statistics, setStatistics] = useState<Statistic[]>();
+    const [bounds, setBounds] = useState<LatLngBounds>();
+    const [zoom, setZoom] = useState(0);
+    const { height, width } = useWindowDimensions();
+
+    const map = useMapEvents({
+        moveend() {
+            setBounds(map.getBounds());
+            setZoom(map.getZoom())
+            console.log(map.getBounds());
+            getStatistics();
+        },
+        zoomend() {
+            setBounds(map.getBounds());
+            setZoom(map.getZoom())
+            console.log(map.getBounds);
+            getStatistics();
+        },
+        resize() {
+            setBounds(map.getBounds());
+            setZoom(map.getZoom())
+            console.log(map.getBounds);
+            getStatistics();
+        }
+    })
+
+    const getStatistics = () => {
+        if (!bounds || !zoom) return;
+
+        const url = "http://localhost:5000/statistics?bounds=" +
+            getLineString(bounds) +
+            "&zoom=" +
+            zoom.toString();
+
+        fetch(url)
+            .then(res => res.json())
+            .then(
+                (statistics: any[]) => {
+                    statistics.map((statistic) => {
+                        statistic.centerPoint = fromPointString(statistic.centerPoint);
+                    });
+
+                    statistics = statistics as Statistic[];
+
+                    setStatistics(statistics);
+                    setIsLoaded(true);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    };
+
+    if (!statistics) return null;
+    const markers = statistics.map((statistic) => <CircleWithNumber width={width} height={height} number={statistic.count} position={[statistic.centerPoint.lat, statistic.centerPoint.lng]}></CircleWithNumber>)
+    return <div>{markers}</div>;
+}
+
 interface MapState {
     width: number;
     height: number;
 }
-class Map extends React.Component<{}, MapState> {
 
-    state = { width: 0, height: 0 };
+const Map = () => {
+    return (
+        <MapContainer center={[51.505, -0.09]} zoom={3} scrollWheelZoom={true} style={{ height: 800, width: "100%" }}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
 
-    constructor(props: {}) {
-        super(props);
-        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-    }
+            <StatisticMarkers />
 
-    componentDidMount() {
-        this.updateWindowDimensions();
-        window.addEventListener('resize', this.updateWindowDimensions);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updateWindowDimensions);
-    }
-
-    updateWindowDimensions() {
-        this.setState({ width: window.innerWidth, height: window.innerHeight });
-    }
-
-
-    render() {
-        const positions: LatLngTuple[] = [[50.5, 30.5], [40.5, 30.5], [50.5, 40.5], [8.5, 30.5], [10.5, 30.5]]
-        const markers = positions.map((position) => <CircleWithNumber width={this.state.width} height={this.state.height} number={32} position={position}></CircleWithNumber>)
-        return (
-            <MapContainer center={[51.505, -0.09]} zoom={3} scrollWheelZoom={true} style={{ height: 800, width: "100%" }}>
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-
-                {markers}
-
-            </MapContainer>
-        )
-    }
+        </MapContainer>
+    )
 }
 
 export default Map
